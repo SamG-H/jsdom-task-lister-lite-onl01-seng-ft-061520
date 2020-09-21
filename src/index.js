d1e84aaf749189bd7c40026afa9d8cafcb236b67
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-09-20 19:33:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    document.querySelector('input[type="submit"]').addEventListener("click", function(event){
+	let taskDescription = document.querySelector('#new-task-description').value;
+	let ul = document.querySelector('#tasks');
+	let li = document.createElement('li');
+	li.innerHTML = taskDescription;
+	ul.appendChild(li);
+	event.preventDefault();
+    });
 });
